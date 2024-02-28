6d6e267124b16dc84550744a7757363d07f9dfc7
meaarushi2002 <meaarushi2002@gmail.com>
2024-02-28 19:14:38 +0530

    complete footer

--- carfe/components/NavBar.tsx
+++ carfe/components/NavBar.tsx
@@ -6,38 +6,39 @@ import CustomButton from './CustomButton'
 const NavBar = () => {
   return (
     <nav className="bg-white h-20 py-2 px-3 flex justify-around items-center">
-      <div className="px-10">
+      <div className="px-10 flex items-center">
         <Link href="/">
-          <Image src="/logo.svg" height={100} width={100} alt="logo" />
+          <Image src="/carzorro_icon.png" height={50} width={50} alt="logo" />
         </Link>
+        <h1 className="font-bold text-2xl text-purple">CARZORRO</h1>
       </div>
       <div className="flex">
         <ul>
-          <li className='text-gray-400 mx-10'>
+          <li className="text-gray-400 mx-5">
             <a href="#!">SERVICES</a>
           </li>
         </ul>
         <ul>
-          <li className='text-gray-400 mx-5'>
+          <li className="text-gray-400 mx-5">
             <a href="#!">DOCUMENTATION</a>
           </li>
         </ul>
         <ul>
-          <li className='text-gray-400 mx-5'>
+          <li className="text-gray-400 mx-5">
             <a href="#!">BLOG</a>
           </li>
         </ul>
         <ul>
-          <li className='text-gray-400 mx-5'>
+          <li className="text-gray-400 mx-5">
             <a href="#!">SUPPORT</a>
           </li>
         </ul>
       </div>
       <div className='flex'>
-        <CustomButton title='Book Demo' btnType='button' containerStyles='text-white rounded bg-blue-400 min-w-[130px] mx-3' />
-        <CustomButton title='Login' btnType='button' containerStyles='text-blue-400 rounded bg-white min-w-[130px] border border-blue-400 mx-3' />
+        <CustomButton title='Book a Demo' btnType='button' containerStyles='font-semibold text-white rounded bg-purple min-w-[130px] mx-3' />
+        <CustomButton title='Login' btnType='button' containerStyles='text-purple rounded bg-white min-w-[130px] border border-purple mx-3' />
       </div>
-    </nav>
+    </nav >
   )
 }
 
